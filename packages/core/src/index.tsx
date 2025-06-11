fee1fcfd752662542c9b67349e2f981ee0ac4f6f
Lee Henson <lee.m.henson@gmail.com>
2025-06-11 15:50:52 +0100

    export NativeLottieAnimationView

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,3 +1,5 @@
 import { LottieView } from './LottieView';
+import NativeLottieAnimationView from "./specs/LottieAnimationViewNativeComponent";
+export { NativeLottieAnimationView };
 export * from './types';
 export default LottieView;
