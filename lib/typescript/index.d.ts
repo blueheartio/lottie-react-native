58320dbad79c5bf6c2fdc857c28ccc7bed531813
Lee Henson <lee.m.henson@gmail.com>
2025-06-12 11:03:47 +0100

    compiled

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,7 @@
+import { LottieView } from './LottieView';
+import NativeLottieAnimationView from "./specs/LottieAnimationViewNativeComponent";
+declare const LottieCommands: import("./specs/LottieAnimationViewNativeComponent").NativeCommands;
+export { LottieCommands, NativeLottieAnimationView };
+export * from './types';
+export default LottieView;
+//# sourceMappingURL=index.d.ts.map
